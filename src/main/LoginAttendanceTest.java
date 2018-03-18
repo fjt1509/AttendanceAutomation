b8bd4756baf444b59df41110bb45b34129d51995
fjt1509 <fjt1509@hotmail.com>
2018-03-19 00:52:54 +0100

    setup

--- src/main/LoginAttendanceTest.java
+++ src/main/LoginAttendanceTest.java
@@ -23,7 +23,7 @@ public class LoginAttendanceTest extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/view/LoginView.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/res/LoginView.css");
